1ef6514954bf25f13c6581ec09f83b9692b37888
JT <jbowler2400@gmail.com>
2015-08-12 21:52:11 -0700

    Basic server running; added some directories for later

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 var express = require('express');
 var app = express();
+var http = require('http').createServer(app);
+var bodyParser = require('body-parser');
+
+app.set('ipaddr', '127.0.0.1');
+app.set('port', 8000);
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Server up and runnin');
+});
+
+http.listen(app.get('port'), app.get('ipaddr'));
