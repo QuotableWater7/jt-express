6f1b578cf118adf21a34790811c6286c8672c672
JT <jbowler2400@gmail.com>
2015-08-12 22:12:13 -0700

    First jade file added

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ var app = express();
 var http = require('http').createServer(app);
 var bodyParser = require('body-parser');
 
+console.log("**** application is running ****");
+
 app.set('ipaddr', '127.0.0.1');
 app.set('port', 8000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.send('Server up and runnin');
+  res.render('index');
 });
 
 http.listen(app.get('port'), app.get('ipaddr'));
