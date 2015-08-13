ee98a437f894b7974bb35344e5d4deaa48c37fe8
JT <jbowler2400@gmail.com>
2015-08-12 21:32:33 -0700

    Clearing out some stuff to make way for a chat app

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
 var express = require('express');
-var hbs = require('hbs');
 var app = express();
-
-app.set('view engine', 'html');
-app.engine('html', hbs.__express);
-
-app.get('/', function (req, res) {
-  res.render('index');
-});
-
-app.get('/blah', function (req, res) {
-  res.end('You are blah-ing');
-});
-
-app.listen(8000);
